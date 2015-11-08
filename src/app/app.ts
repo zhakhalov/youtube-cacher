15df41afd98541b5fcaca719fb91708bda2aa05e
zhakhalov <zhakhalov@gmail.com>
2015-11-08 14:49:33 +0200

    ionic material restores

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,6 @@ namespace app {
       'ionic',
       'ionic-material',
       'ionMdInput',
-      // 'ngMaterial'
     ];
     private static __context: angular.IModule = angular.module('app', Application.$inject);
     public static get context(): angular.IModule { return Application.__context; }
